b7efc14a60b99f7e7c50bad9ed42d86cc60b0a6d
github username <github email address>
2015-11-11 12:27:37 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,12 +6,10 @@ def batch_badge_creator(attendees)
   attendees.map { |a| badge_maker(a) }
 end
 
-def assign_rooms(attendees) 
-  arr = []
-  attendees.each_with_index do |name, index|
-    arr << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
-  arr
 end
 
 def printer(attendees)
