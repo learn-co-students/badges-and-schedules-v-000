b7659e14e873d0355dba3b1ec514c207da6aadd0
authorbeard <msnolan@gmail.com>
2015-11-19 11:53:47 -0500

    all tests passing.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+# require "pry"
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(attendees)
+  room_assignments=[]
+  attendees.each_with_index {|name, room| room_assignments << "Hello, #{name}! You'll be assigned to room #{room + 1}!"}
+  room_assignments
+end
+
+def printer(attendees)
+  # binding.pry
+  batch_badge_creator(attendees).each {|badge| puts badge }
+  assign_rooms(attendees).each {|room| puts room}
+  
+end
+
+
+
