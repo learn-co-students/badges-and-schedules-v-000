306c9476997f39de57255aab63dcb3b8a7147f66
pacosta613 <pedro.acosta613@gmail.com>
2015-12-16 10:17:15 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  return badges
+end
+
+def assign_rooms(attendees)
+  return room_assignments
+end
+
+def printer(attendees)
+  badges_and_room_assignments.each_line do |line|
+    puts line.chomp
+   end
+end
