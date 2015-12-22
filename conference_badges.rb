c0effe6f6fb1bada655f0bd5e4c0b87452eddafd
glee38 <glee3388@gmail.com>
2015-12-21 20:58:38 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,15 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+#collect method will return a new array with the results of running block once for every element
+
 def batch_badge_creator(speakers)
  speakers.collect { |speaker| "Hello, my name is #{speaker}."}
 end
+
+#made a new array in which to push room assignments into so that it will return the new array instead of the original
+
+=begin
 
 def assign_rooms(speakers) 
   room_assignments = []
@@ -15,7 +21,20 @@ def assign_rooms(speakers)
   room_assignments
 end
 
+=end
+
+#refactored above code by chaining methods, #collect & #each_with_index
+
+def assign_rooms(speakers) 
+  speakers.each_with_index.collect do |speaker,room|
+    "Hello, #{speaker}! You'll be assigned to room #{room + 1}!"
+  end
+end
+
+#called two methods within #printer. 
+#the argument variable for #printer must be the same as #batch_badge_creator and #assign_rooms
+
 def printer(speakers)
   batch_badge_creator(speakers).each { |speaker| puts speaker}
-  assign_rooms(speakers).each { |speaker| puts speaker}
+  assign_rooms(speakers).each { |room| puts room}
 end
