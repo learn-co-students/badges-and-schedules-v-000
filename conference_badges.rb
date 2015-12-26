595e0f82f631f355285046a4e24d06dc327e85c7
Chris Bruen <bruen.chris@gmail.com>
2015-12-26 10:10:45 -0500

    added methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name)
+	return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+	names.collect! {|x| "Hello, my name is #{x}." }
+	names
+end
+
+def assign_rooms(names)
+	names.collect! {|x| "Hello, #{x}! You'll be assigned to room #{names.index(x)+1}!" }
+	names
+end
+
+def printer(names)
+	batch_badge_creator(names).each { |x| puts x }
+	assign_rooms(names).each { |x| puts x }
+end
