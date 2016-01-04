7f1988120c02a24551aa020fced9eab58bcacd61
adamgfraser <adam.fraser@gmail.com>
2016-01-04 14:59:55 -0800

    added conference badge methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(names)
+  names.each_with_index.map do |name, room|
+    "Hello, #{name}! You'll be assigned to room #{room+1}!"
+  end
+end
+
+def printer(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
+  assign_rooms(names).each do |room_assignment|
+    puts room_assignment
+  end
+end
