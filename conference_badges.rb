d56456ef4b66c346b86329756ccb279ee696692b
Chris-Sexton <champlain2013@gmail.com>
2016-02-08 10:25:34 -0500

    Badges and schedules lab complete

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+ def badge_maker(name)
+    "Hello, my name is #{name}."
+  end
+
+  def batch_badge_creator(attendees)
+    attendees.map do |name|
+       "Hello, my name is #{name}."
+    end
+  end
+
+  def assign_rooms(attendees)
+    attendees.collect.with_index do |name, room|
+      "Hello, #{name}! You'll be assigned to room #{room+1}!"
+    end
+  end  
+
+  def printer(attendees)
+    batch_badge_creator(attendees).each do |name|
+      puts name
+    end
+    assign_rooms(attendees).each do |name|
+      puts name
+    end
+  end  
