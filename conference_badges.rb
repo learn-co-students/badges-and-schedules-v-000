9440a0193d00c8290e9599997b2e6dc5161c4589
CodeByLine <yleventhal@ymail.com>
2016-02-15 01:46:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+  name_array =[]
+  puts "Hello, my name is #{name}."
+  name_array << name
+end
+
+def batch_badge_creator(name_array)
+
+  name_array =[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+  name_array.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(name_array)
+  name_array =[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+  name_array.each do |name|
+    puts "Hello, #{name}! You'll be assigned to room #{name_array[name]}!"
+  end
+end
+
+def printer
+  batch_badge_creator(name_array)
+  assign_rooms(name_array)
+end
