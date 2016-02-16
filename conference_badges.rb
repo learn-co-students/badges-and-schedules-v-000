12b5efa2cef0d77151ce4e23a688fff1b204eaea
Brian Holland <brian.holland@proatria.com>
2016-02-16 17:22:30 -0600

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list)
+  return list.map{|x| "Hello, my name is " + x + "."}
+end
+
+def assign_rooms(list)
+  return list.map.with_index{|x, i| "Hello, " + x + "! You'll be assigned to room #{i+1}!"}
+end
+
+def printer(list)
+  badges = batch_badge_creator(list)
+  rooms = assign_rooms(list)
+  badges.each{|badge| puts badge}
+  rooms.each{|room| puts room}
+end
