271404e25ab79359bac9677b743f808e18fd098d
cernanb <cernanb@gmail.com>
2016-02-20 21:59:35 -0800

    created the methods to create badges and assign rooms

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  batch_badge = []
+  names.each do |name|
+    batch_badge << badge_maker(name)
+  end
+  batch_badge
+end
+
+def assign_rooms(names) 
+  rooms_array = []
+  names.each_with_index do |name, index|
+    rooms_array << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  rooms_array
+end
+
+def printer(names)
+  batch = batch_badge_creator(names)
+  batch.each do |i|
+    puts i
+  end
+  rooms = assign_rooms(names)
+  rooms.each do |i|
+    puts i
+  end
+end
