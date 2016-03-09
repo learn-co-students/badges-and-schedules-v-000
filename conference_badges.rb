28883c9d24d67ebd744a494f2b743134af9d94a2
hangloos <ericloos00@gmail.com>
2016-03-09 18:28:18 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,41 @@
 # Write your code here.
+
+def badge_maker(name)
+
+  return "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(attendees)
+
+  badges = []
+
+  attendees.each {|x|  badges << badge_maker(x) }
+
+  badges
+
+end
+
+def assign_rooms(attendees)
+  
+  rooms = []
+
+  attendees.each_index {|x| rooms << "Hello, #{attendees[x]}! You'll be assigned to room #{x+1}!"}
+  rooms
+  
+end
+
+def printer(attendees)
+  
+  batch_badge_creator(attendees).each{|x| puts x}
+  assign_rooms(attendees).each{|x| puts x}
+  
+
+
+end
+
+#.join(",")
+
+## rand(1,7)
+
+# each_index and then + 1 for the room number. then use same index for the name
