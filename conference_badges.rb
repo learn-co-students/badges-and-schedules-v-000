855185e4abd175049ddea9991312100c1f188fbd
PeteAJ <peter.jacobson@gmail.com>
2016-03-13 13:24:37 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(name, room_number)
+    name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+     room_number = ["1", "2", "3", "4", "5", "6", "7"]
+
+  puts "Hello, #{name}, you are in #{room_number}."
+end
+
