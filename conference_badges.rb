cfad2c096990f4efe5ed7dca612eeb9454385021
Being Young <beingy@gmail.com>
2016-05-01 12:17:46 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,22 +3,16 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  badge_messages = []
-  attendees.each { |attendee| badge_messages << badge_maker(attendee) }
-  badge_messages
+  attendees.collect { |attendee| badge_maker(attendee) }
 end
 
 def assign_rooms(attendees)
-  room_assignments =[]
-  attendees.each_with_index do |attendee, index|
-    room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+  attendees.each_with_index.collect do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
   end
-  room_assignments
 end
 
 def printer(attendees)
-  badges = batch_badge_creator(attendees)
-  room_assignments = assign_rooms(attendees)
-  badges.each {|badge| puts badge}
-  room_assignments.each {|room| puts room}
+  batch_badge_creator(attendees).each { |badge_message| puts badge_message }
+  assign_rooms(attendees).each { |room_assignment| puts room_assignment }
 end
