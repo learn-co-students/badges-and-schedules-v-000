8c4528ac2358f626f011b7b6049665808dae7282
Anita Ford <anitamford@gmail.com>
2016-05-09 21:17:56 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array_msgs = []
+  array.each do |names|
+    array_msgs << "Hello, my name is #{names}."
+  end
+  return array_msgs
+end
