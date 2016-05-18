3b2b2c65953dc2152fc9687b6ce42eae290a9712
tpstar <hanlee12@gmail.com>
2016-05-17 22:40:52 -0400

    solve badges and schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name)
+	"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+	names.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(speakers)
+	speakers.collect {|speaker| "Hello, #{speaker}! You'll be assigned to room #{speakers.index(speaker) + 1}!"}
+end
+
+def printer(names)
+	batch_badge_creator(names).each {|x| puts x}
+	assign_rooms(names).each {|x| puts x}
+end
