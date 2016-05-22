b03c01bae649785dedbda91eb5b3b50a5c1670af
jack woodward <jackwoodwardschool@gmail.com>
2016-05-22 16:11:40 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |attendee|
+        badges.push(badge_maker(attendee))
+    end
+    return badges
+end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    attendees.each_with_index do |attendee, index|
+        room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{index + 1}!")
+    end
+    return room_assignments
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees)
+    badges.each do |attendee|
+        puts attendee
+    end
+    assign_rooms(attendees)
+    room_assignments.each do |attendee|
+        puts attendee
+    end
+end
