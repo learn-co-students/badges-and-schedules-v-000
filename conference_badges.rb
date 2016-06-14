48e7d959ae43ab96fe343f0e837885fb5a3984c3
Jesse Novotny <jessenovotny@gmail.com>
2016-06-14 16:04:38 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,9 +12,20 @@ end
 
 def assign_rooms(attendees)
   room_assignments = []
-  room_number = 0
-  assign_rooms.each do |variable|
-    room_assignments.push("Hello, #{variable}! You'll be assigned to room " + (room_number + 1) + "!")
+  room_number = 1
+  attendees.each do |variable|
+    room_assignments.push("Hello, #{variable}! You'll be assigned to room #{room_number}!")
+    room_number += 1
   end
   room_assignments
 end
+
+def printer(attendees)
+  badges.each do |variable|
+    puts "#{variable}"
+  end
+  room_assignments.each do |variable|
+    puts "#{variable}"
+  end
+end
+
