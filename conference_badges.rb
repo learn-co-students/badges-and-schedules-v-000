3c44ba6e9365eef4376dbbe22d11f97465aea1cf
Scott Savage <snsavage@gmail.com>
2016-06-15 22:51:20 -0400

    Passing tests for conference badges.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(attendees)
+  attendees.map.with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |output|
+    puts output
+  end
+
+  assign_rooms(attendees).each do |output|
+    puts output
+  end
+end
+
