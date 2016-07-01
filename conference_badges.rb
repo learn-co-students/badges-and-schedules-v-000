da66d44fc36be9d94538c9189a11d99f2da82757
vanita eckert <vanita112003@gmail.com>
2016-07-01 22:15:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
+require 'pry'
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(names)
+  names = []
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+    names.each do |speaker|
+    badge_maker
+binding.pry
+  end
+end
