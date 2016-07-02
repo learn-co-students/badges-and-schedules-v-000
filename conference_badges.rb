541673ccfc9d098b37aeebc93d2e352d765ca6de
vanita eckert <vanita112003@gmail.com>
2016-07-02 21:55:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,11 +5,24 @@ def badge_maker(name)
 end
 
 
-def batch_badge_creator(names)
-  names = []
-  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-    names.each do |speaker|
-    badge_maker
-binding.pry
+def batch_badge_creator(name)
+  name = []
+  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+    name.collect do |speaker|
+    "Hello, my name is #{speaker}."
+  end
+
+end
+
+def assign_rooms(names)
+  names.collect.with_index do |name, room|
+    "Hello, #{name}! You'll be assigned to room #{room+1}!"
+  end
+end
+
+def printer(name)
+  name.each do |name, names|
+    batch_badge_creator(name)
+    assign_rooms(names)
   end
 end
