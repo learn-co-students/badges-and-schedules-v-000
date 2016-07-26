0b39d7fbe5b70e74b0cc4e2447a7adf373b8148a
William Jones <enquiries@forestfed.com>
2016-07-26 13:36:38 +0000

    use .each and .each_with_index methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  return_array = []
+  attendees.each do |name|
+    return_array << badge_maker(name)
+  end
+  return_array
+end
+
+def assign_rooms(attendees)
+  return_array = []
+  attendees.each_with_index do |name, room|
+    return_array << "Hello, #{name}! You'll be assigned to room #{room+=1}!"
+  end
+  return_array
+end
+
+def printer(attendees)
+  return_array = batch_badge_creator(attendees)
+  return_array.each do |message|
+    puts "#{message}"
+  end
+  return_array = assign_rooms(attendees)
+  return_array.each do |message|
+    puts "#{message}"
+  end
+end
