cfe365b816a3f231e26cef5cfb7ac983cfa0d58b
Janine Rosen <janinemrosen@gmail.com>
2016-09-07 19:57:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,11 +13,17 @@ end
 
 def assign_rooms(attendees)
   room = 1
+  new_array = Array.new
   attendees.each do |attendee|
-    puts "Hello, #{attendee}! You'll be assigned to room #{room}!"
+    new_array << "Hello, #{attendee}! You'll be assigned to room #{room}!"
     room += 1
   end
+  return new_array
 end
 
-def printer
+def printer(attendees)
+  assign_rooms(attendees).each do |attendee|
+    puts attendee
+  end
+  batch_badge_creator(attendees)
 end
