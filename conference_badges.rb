35736a9994d0ca5d7f71b6f3afb9a69ec598bcd0
Chirag Dudhat <cdudhat@gmail.com>
2016-09-10 04:59:56 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ end
 
 def batch_badge_creator(array)
   array.collect {|x| "Hello, my name is #{x}."}
-=begin
+=begin # Alternative method to complete the same task
   counter = 0
   badge_list = []
   array.each do |x|
@@ -17,7 +17,7 @@ end
 
 def assign_rooms(array)
   array.collect.with_index {|x, index| "Hello, #{x}! You'll be assigned to room #{index+1}!"}
-=begin
+=begin #Alternative Method to complete the same task
   room = 1
   room_assignments = []
   array.each do |x|
@@ -29,7 +29,8 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  #array.each {|x| puts "Hello, my name is #{x}."}
   batch_badge_creator(array).each {|x| puts x}
+  #array.each {|x| puts "Hello, my name is #{x}."} #Alternative method but doesnt make use of previous method
   assign_rooms(array).each {|x| puts x}
+  # Above mentioned alternative method can be applied here too...but rather not
 end
