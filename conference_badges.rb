08e07a641ec46d5f21fa9e963a8cb2893db43c9e
Scott Wakeman <swakeman@ca.rr.com>
2016-09-25 14:41:42 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each do
+    "Hello, my name is #{attendees}."
+  end
+  badges
+end
+
+def assign_rooms(attendees, room)
+  room = [1,2,3,4,5,6,7]
+  attendees.each do
+  room.each do
+  puts "Hello, #{attendees}! You'll be assigned to room #{rooms}!"
+  end
+  end
+  room_assignments
+end
