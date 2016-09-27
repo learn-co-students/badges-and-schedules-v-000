effd30fce334a799d43ee355d10ab47906205aa8
Scott Wakeman <swakeman@ca.rr.com>
2016-09-27 15:36:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,22 +1,39 @@
+#require 'pry'
 # Write your code here.
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  attendees.each do
-    "Hello, my name is #{attendees}."
-  end
-  badges
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees.each do
+  "Hello, my name is #{attendees}."
+end
+badges
 end
 
+#attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+#attendees.collect {|badges|puts"Hello, my name is #{attendees}."}
+#end
+
 def assign_rooms
-                                                  #attendees.each do
-  (1..7).each attendees do |room_assignments|  #room = [1,2,3,4,5,6,7]
-                                              #%w(room_assignments).each_with_index { |attendees, room|
-                                              # =>assign_rooms[attendees] = rooms
-    "Hello, #{attendees}! You'll be assigned to room #{room_assignments}!"
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+(1..7).each attendees do |room_assignments|
   end
+    "Hello, #{attendees}! You'll be assigned to room #{room_assignments}!"
   room_assignments
 end
+
+#hash=hash.new
+#%w(Edsger, Ada, Charles, Alan, Grace, Linus, Matz).each_with_index { |item, index|
+#hash[item] = index
+
+#%w(room_assignments).each_with_index { |attendees, room|}
+# =>assign_rooms[attendees] = rooms
+
+#%w(attendees).each_with_index { |rooms, index|
+#attendees[item] = index
+
+
+#binding.pry
+#1+1
