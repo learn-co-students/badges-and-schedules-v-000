a1d7ef9a7a1bcb4d2265dd65c98fc29ac4285cee
Scott Wakeman <swakeman@ca.rr.com>
2016-09-27 02:44:45 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,12 +11,12 @@ def batch_badge_creator(attendees)
   badges
 end
 
-def assign_rooms#(attendees)
-  attendees.each do
-    #(1..7).each attendees do |room_assignments|  #room = [1,2,3,4,5,6,7]
+def assign_rooms
+                                                  #attendees.each do
+  (1..7).each attendees do |room_assignments|  #room = [1,2,3,4,5,6,7]
                                               #%w(room_assignments).each_with_index { |attendees, room|
                                               # =>assign_rooms[attendees] = rooms
-    "Hello, #{attendees}! You'll be assigned to room #{rooms}!"
+    "Hello, #{attendees}! You'll be assigned to room #{room_assignments}!"
   end
   room_assignments
 end
