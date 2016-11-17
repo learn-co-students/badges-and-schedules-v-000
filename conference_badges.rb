a17375b91efbfea7d954e72b7881eae5d051b50a
Daniel Warren <warrend555@gmail.com>
2016-11-17 14:24:34 -0500

    finish badges and schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  badge_messages = []
+  speakers.each do |speaker|
+    badge_messages << "Hello, my name is #{speaker}."
+  end
+  badge_messages
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  number_of_rooms = 7
+  speakers.each_with_index do |speaker, room|
+    room_number = ((room + speakers.count - number_of_rooms) % number_of_rooms) + 1
+    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+  end
+  room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+  assign_rooms(attendees).each do |attendee|
+    puts attendee
+  end
+end
