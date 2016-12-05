3f35981893b43f5fce9270021288d966bd2d9728
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-12-05 13:25:44 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+speakers = []
+  attendees.each do |attendee|
+speakers.push("Hello, my name is #{attendee}.")
+  end
+return speakers
+end
 # Write your code here.
+
+
+#primary_colors = ["Red", "Yellow", "Blue"]
+#primary_colors.each do |color|
+#  puts "Primary Color #{color} is #{color.length} letters long."
+#end
