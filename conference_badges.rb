8b1b07a424c7daf474847a5b18c5c1c549c4a917
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-12-05 15:33:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,15 @@ speakers.push("Hello, my name is #{attendee}.")
   end
 return speakers
 end
+
+def assign_rooms(attendees)
+  speakers = []
+  attendees.each_with_index do |attendee, index|
+  speakers.push("Hello, #{attendee}! You'll be assigned to room #{index+1}!")
+end
+return speakers
+end
+
 # Write your code here.
 
 
