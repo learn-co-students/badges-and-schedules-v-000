10d6aa09b7d0060f3c9d6c510ac14db629f78733
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-01-28 23:23:29 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
+require 'pry'
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(speakers)
+  speakers.each_with_index.collect do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(speakers)
+  badges = batch_badge_creator(speakers)
+  badges.each do |badge|
+    puts badge
+  end
+  assignments = assign_rooms(speakers)
+  assignments.each do |assign|
+    puts assign
+  end
+end
+
+binding.pry
