aeefc39b031caef7885f1f3af9194c6fbdfc2439
Erika Hughes <ehughes@knights.ucf.edu>
2017-02-07 19:26:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(batch)
+  badges = []
+  batch.each do |x|
+    badges << badge_maker(x)
+  end
+  return badges
+end
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index do |speaker, index|
+    rooms <<  "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+  end
+  return rooms
+end
+
+def printer(attendees)
+  printed = batch_badge_creator(attendees)
+  printed.each do |x|
+    puts x
+  end
+  assigned = assign_rooms(attendees)
+  assigned.each do |x|
+    puts x
+  end
+end
