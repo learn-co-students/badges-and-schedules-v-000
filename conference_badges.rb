6387318d2d4bc6fb3ddea9e99ae98272f6392a8e
thesilentc <thesilentc@me.com>
2017-02-28 21:22:41 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,8 @@ end
 def assign_rooms(speakers)
   attendees.collect {|speaker| "Hello, #{speaker}! You'll be assigned to room #{speakers.index(speaker)+1}!"}
 end
+
+def printer(names)
+  batch_badge_creator(names).each {|item| puts item}
+  assign_rooms(names).each {|room_num| puts room_num}
+end
