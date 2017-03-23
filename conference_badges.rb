e4c0fc78d6f633771989c810dbb5f078fec316e8
Danielle Croxton <danikrist@gmail.com>
2017-03-23 23:18:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  name.collect{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(name)
+  name.each_with_index.map do | name, index |
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(name)
+  batch_badge_creator(name).each do |badge|
+    puts badge
+  end
+  assign_rooms(name).each do |room|
+    puts room
+  end
+end
