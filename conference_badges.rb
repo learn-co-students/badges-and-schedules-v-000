8a72fad17c71370ba66cc4cda00f75e1295efbe0
David Mars <david.w.mars@my.occc.edu>
2017-04-01 06:49:52 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+#~~~HAVICK WAS HERE~~~
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+badgeMade=[]
+   array.each {|name| badgeMade << badge_maker(name)}
+return badgeMade
+end
+
+def assign_rooms(array)
+  rooms = []
+  array.each.with_index(1) do |name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index}!"
+    
+  end
+  return rooms
+end
+
+def printer(attendees)
+batch_badge_creator(attendees).each {|name| puts name}
+assign_rooms(attendees).each {|name| puts name}
+
+
+end
