89f5a9b5dd98a826cecb5bb3d6cb47adaf513b07
Rebecca Bratu <beckybratu@gmail.com>
2017-04-13 02:20:31 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |name|
+    badges << badge_maker(name)
+  end
+    badges 
+end 
+
+require 'enumerator'
+
+def assign_rooms(attendees)
+    attendees.enum_for(:each_with_index).collect {|name, index| 
+    puts "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    }
+end 
+
+def printer(attendees)
+  attendees.each do |attendee|
+    puts batch_badge_creator(attendee)
+  end 
+end
+
