525617e33b2164db5b17a24af313b0aa503b372b
lucketta <lucketta@umich.edu>
2017-04-16 23:44:26 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect {|badge| badge_maker(badge)}
+end
+
+def assign_rooms(attendees)
+  attendees.collect.each_with_index { |badge, index| "Hello, #{badge}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer(attendees)
+  list = batch_badge_creator(attendees)
+  list.each {|badge| puts badge}
+  new_list = assign_rooms(attendees)
+  new_list.each {|badge| puts badge}
+
+end
