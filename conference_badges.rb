62592de422ed14cb5ab979115e379cf85e4b2e3b
Will Sauer <will@heueso.com>
2017-04-18 20:48:17 +0000

    finished

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+
+
+
+def batch_badge_creator (array)
+  array.collect{|x|
+  puts "Hello, my name is #{x}."
+  "Hello, my name is #{x}."
+  }
+end
+
+
+
+
+def assign_rooms (array)
+  array.each_with_index.collect { |item,index|
+  puts "Hello, #{item}! You'll be assigned to room #{index+1}!"
+  "Hello, #{item}! You'll be assigned to room #{index+1}!"
+  }
+end
+
+
+def printer (array)
+  batch_badge_creator(array)
+  assign_rooms(array)
+end
