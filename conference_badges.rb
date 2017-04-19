52fb8a5120e4228209bc4a55184be0ac555d24bc
Jeremy Emata <emata@rapidratings.com>
2017-04-19 22:50:16 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end# Write your code here.
+
+def batch_badge_creator(array)
+  array.map do |individual_names|
+    "Hello, my name is #{individual_names}."
+    
+  end
+end
+
+def assign_rooms(speaker_array)
+  speaker_array.each_with_index.map do |name,i|
+  "Hello, #{name}! You'll be assigned to room #{i+1}!"
+end
+end
+
+def printer (attendees)
+  batch_badge_creator(attendees).each do |badge|
+      puts badge
+    end
+  assign_rooms(attendees).each do |name|
+      puts name
+  end
+end
