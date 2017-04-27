57e699c45fb88c801300448f8fd6f39f752e7a15
Alex Jelly <alexjelly8@gmail.com>
2017-04-27 02:35:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,42 +1,62 @@
 # Write your code here.
 
-speakers = ["Edsger", "Tom"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
+def batch_badge_creator(attendees)
   badges = []
-  speakers.each do  |x|
-      badges << "Hello, my name is #{x}."
+  attendees.map do  |x|
+      "Hello, my name is #{x}."
   end
-  return badges
 end
 
+#my code was the following
+#def batch_badge_creator(attendees)
+#  badges = []
+#  attendees.each do  |x|
+#      badges << "Hello, my name is #{x}."
+#  end
+#  return badges
+#end
 
-def assign_rooms(speakers)
-  rooms = [1,2,3,4,5,6,7]
-  room_index = 0
-  counter = 0
-  collect = []
-  while counter < speakers.count
-  
-      speakers.each do |y|
-        room = rooms[room_index]
-            collect << "Hello, #{y}! You'll be assigned to room #{room}!"
-        room_index +=1
-        counter +=1
-      end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+      "Hello, #{attendee}! YOu'll be assigned to room #{index+1}"
   end
-  return collect
 end
 
-def printer(speakers)
-  puts batch_badge_creator(speakers)
-  puts assign_rooms(speakers)
-end
+#my code was the following
+#def assign_rooms(attendees)
+#  rooms = [1,2,3,4,5,6,7]
+#  room_index = 0
+#  counter = 0
+#  collect = []
+#  while counter < attendees.count
+#
+#      attendees.each do |y|
+#        room = rooms[room_index]
+#            collect << "Hello, #{y}! You'll be assigned to room #{room}!"
+#        room_index +=1
+#        counter +=1
+#      end
+#  end
+#  return collect
+#end
 
-#puts batch_badge_creator(speakers)
-#puts assign_rooms(speakers)
-printer(speakers)
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
+#my code
+#def printer(attendees)
+#  puts batch_badge_creator(attendees)
+#  puts assign_rooms(attendees)
+#end
