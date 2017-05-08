f00acfc94315a9edc95f9cfc012a4b6dbf85d538
Mason Ellwood <masonellwood@gmail.com>
2017-05-08 00:50:44 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(fuck)
+  fuck.each do |fuckme|
+    "Hello, my name is #{fuckme}."
+  end
+end
