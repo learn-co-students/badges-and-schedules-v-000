98652b8dbdf3a2789042a6518f35746a53817977
gb23 <gregbenj@hotmail.com>
2017-05-17 08:14:05 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,19 +5,15 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  badge_array = []
-  array.each{|element|
-    badge_array << badge_maker(element)
+  array.collect{|element|
+    badge_maker(element)
   }
-  badge_array
 end
 
 def assign_rooms(array)
-  array_r = []
-  array.each_with_index{ |element, index|
-    array_r << "Hello, #{element}! You'll be assigned to room #{index + 1}!"
+  array.each_with_index.collect { |element, index|
+    "Hello, #{element}! You'll be assigned to room #{index+1}!"
   }
-  array_r
 end
 
 
