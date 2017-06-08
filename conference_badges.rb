b5123dcf751a72dbe7c64763ffaaaec1e18be227
Seb Gertz <sebgertz@gmail.com>
2017-06-08 14:40:22 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz.]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(names)
+  names.each do |name|
+    badge_maker(name)
+  end
+end
