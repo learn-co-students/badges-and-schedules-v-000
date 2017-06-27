28dd2e3b49f5f05478c787916f44bc8914d6f03a
Karen Berba <kberba@gmail.com>
2017-06-27 03:28:03 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+#badge_maker -- should return a formatted badge
+def badge_maker( name )
+  return "Hello, my name is #{name}."
+end
+
+#batch_badge_creator -- should return a list of badge messages
+def batch_badge_creator( attendees )
+  attendees.map do |attendee|
+    puts "Hello, my name is #{attendee}."
+  end
+  #badge_maker( attendees.each { |name| print name } )
+end
