ef6465948477a4cec9d1e9bd0f23316182e703cd
Sydney Chun <schun08@gmail.com>
2017-07-02 22:10:35 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,22 +1,49 @@
+# first attempt at the lab (6/13/17)
+# def badge_maker(name)
+#   "Hello, my name is #{name}."
+# end
+#
+# def batch_badge_creator(attendees)
+#   res = []
+#   attendees.each {|person| res.push(badge_maker(person))}
+#   return res
+# end
+#
+# def assign_rooms(attendees)
+#   res = []
+#   attendees.each_with_index do |person, index|
+#     res.push("Hello, #{person}! You'll be assigned to room #{index+1}!")
+#   end
+#   return res
+# end
+#
+# def printer(attendees)
+#   attendees.each {|person| puts badge_maker(person)}
+#   assign_rooms(attendees).each {|line| puts line}
+# end
+
+# second attempt at the lab (7/2/17)
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-  res = []
-  attendees.each {|person| res.push(badge_maker(person))}
-  return res
+  attendees.collect do |name|
+    badge_maker(name)
+  end
 end
 
 def assign_rooms(attendees)
-  res = []
-  attendees.each_with_index do |person, index|
-    res.push("Hello, #{person}! You'll be assigned to room #{index+1}!")
+  attendees.each_with_index.collect do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
-  return res
 end
 
 def printer(attendees)
-  attendees.each {|person| puts badge_maker(person)}
-  assign_rooms(attendees).each {|line| puts line}
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end
