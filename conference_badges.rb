5cadb0aa5a2d91bb02ac179c3fdf7505e8c4a2c3
Mariel F <marielbeth@gmail.com>
2017-07-22 14:13:36 +0000

    created array methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index do |speaker, idx|
+    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{idx + 1}!"
+  end
+  room_assignments
+end
+
+def printer(speakers)
+  badges = batch_badge_creator(speakers)
+  badges.each {|badge| puts badge}
+  rooms = assign_rooms(speakers)
+  rooms.each {|room| puts room}
+end
