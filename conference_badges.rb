edfc1a0af5b4eda651a75ae4b0184420b597439b
brandon wirz <brandonwirz2@brandons-MacBook-Pro.local>
2017-08-17 16:01:20 -0600

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee| "Hello, my name is #{attendee}" }
+  end
+end
