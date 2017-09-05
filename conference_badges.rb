eda8271f930b3b53c9507d23d3612ca67dce0ec3
Sophie Chang <soapycee@icloud.com>
2017-09-05 19:12:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  newarray = []
+  names.each do |named| newarray <<
+    "Hello, my name is #{named}."
+  end
+  newarray
+end
+
+def assign_rooms(speakers)
+  newarray = []
+    speakers.each_with_index{ |speaker, index| newarray << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
+  newarray
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each {|element| puts element}
+  assign_rooms(speakers).each {|element| puts element}
+end
