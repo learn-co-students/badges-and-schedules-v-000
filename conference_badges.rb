cb17080039ce07679b2edc5617e751a9fdd8150c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-09-13 18:41:48 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  batch_names = []
+  name_array.each do |name|
+    batch_names.push(badge_maker(name))
+  end
+  return batch_names
+end
+
+def assign_rooms(speakers_array)
+  counter = 0
+  room_assignments = []
+  speakers_array.each do |name|
+    counter += 1
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+  end
+  return room_assignments
+end
+
+def printer(attendees)
+    batches = batch_badge_creator(attendees)
+    assigns = assign_rooms(attendees)
+    batches.each do |person|
+      puts person
+    end
+    assigns.each do |room|
+      puts room
+    end
+end
