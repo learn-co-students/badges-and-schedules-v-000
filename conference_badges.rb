310ab71561d09f82eb645e62f28520e718b4101c
Emily Zhao <tong.zhao.92@gmail.com>
2017-09-16 12:26:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+conference_speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(array=conference_speaker)
+  array.collect do |speaker|
+    "Hello, my name is #{speaker}."
+  end
+end
+
+def assign_rooms (speaker_array=conference_speaker)
+  conference_room=[]
+  each_with_index = 0
+  speaker_array.collect do |speaker|
+    each_with_index+=1
+    "Hello, #{speaker}! You'll be assigned to room #{each_with_index}!"
+  end
+end
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def printer(attendees=gets.chomp)
+  printer_attendees=[]
+  printer_attendees<<attendees
+  return batch_badge_creator(printer_attendees)
+end
