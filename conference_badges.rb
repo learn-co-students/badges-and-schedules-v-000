d803e86af96f1fe1920a5c821c47a84f19b0c087
Melissa Rudick <mel.madrugada@gmail.com>
2017-09-18 02:06:44 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,16 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-attendees = ["Edsger", "Ada", "Charles", "Alan", Grace", "Linus", "Matz"]
+# attendees = ["Edsger", "Ada", "Charles", "Alan", Grace", "Linus", "Matz"]
 def batch_badge_creator(list)
   list.map do |attendee|
+    "Hello, my name is #{attendee}."
 end
+end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def assign_rooms(attendee)
+  attendees.each_with_index do |attendee, number|
+    "Hello, #{attendee}! You'll be assigned to room #{number}!"
+  end
 end
