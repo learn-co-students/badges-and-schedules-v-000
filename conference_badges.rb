c296c24a9b2d14ace7e9b03a0c97f675f417e432
Lisa Lee <lisa_lee@brown.edu>
2017-09-25 01:48:12 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list)
+  list.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(list)
+  list.collect.each_with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(list)
+  batch_badge_creator(list).each do |word|
+    puts word
+  end
+  assign_rooms(list).each do |word|
+    puts word
+  end
+end
