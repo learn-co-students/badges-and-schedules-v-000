0b422dd1fd3e0277ad9224d07b5dfb48d32e6108
Richard Hanolt <rhanolt@gmail.com>
2017-10-06 23:28:28 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,6 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  counter = 0
   rooms = Array.new
   attendees.each do |name|
     rooms.push("Hello, #{name}! You'll be assigned to room #{attendees.index(name)+1}!")
@@ -21,6 +20,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-    puts batch_badge_creator(attendees)
-    puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each_with_index { |greeting, index|
+      puts greeting
+      puts assign_rooms(attendees)[index]
+  }
 end
