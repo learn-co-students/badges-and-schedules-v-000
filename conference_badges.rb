1bdef5d5a3f8078b748f19c2666dab576aaad4c9
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 16:18:57 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,16 +3,14 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(list)
-  list.collect {|index| "Hello, my name is #{index}."}
+def batch_badge_creator(attendees)
+  attendees.collect {|attendee| "Hello, my name is #{attendee}."}
 end
 
-def assign_rooms(list)
-  room_assignments = Array.new
-  list.each_with_index {|name, index|
-    room_assignments << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+def assign_rooms(attendees)
+  attendees.map.with_index {|attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   }
-  room_assignments
 end
 
 def printer(attendees)
