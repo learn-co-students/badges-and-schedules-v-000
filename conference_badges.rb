7ef64ff71d6f953281eca62346af894a94949d67
github username <github email address>
2017-11-27 17:38:12 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator("Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz")
+  puts "Hello, my name is []."
+end
