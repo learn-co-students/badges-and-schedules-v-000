dcd563fa806647e09c84b86bae8a4107c9b1f40e
jtorre20 <jtorres1989@gmail.com>
2017-12-08 09:52:53 -0500

    finished all methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+require 'pry'
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+    name_array = []
+    array.each do |x|
+    name_array << "Hello, my name is #{x}."
+    end
+    name_array  
+end
+
+def assign_rooms(attendes)
+    room_arr = []
+    attendes.each_with_index do |name, idx|
+        idx += 1
+        room_arr << "Hello, #{name}! You'll be assigned to room #{idx}!"
+    end
+    room_arr
+end
+
+def printer(array)
+      batch_badge_creator(array).each do |x|
+        puts x
+      end
+
+      assign_rooms(array).each do |y|
+        puts y
+      end
+      
+end
+
+
