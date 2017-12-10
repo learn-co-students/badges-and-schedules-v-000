adc16ff6cd29b77f73f9d1ff8621a956209fa604
Robert Watson <spartan83@gmail.com>
2017-12-10 04:30:08 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+def assign_rooms (attendees)
+room = Array.new
+index = 0
+attendees.each do |room_assignment|
+  room[index] = "Hello, #{room_assignment}! You'll be assigned to room #{index+1}!"
+  index +=1
+end
+room
+end
+
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+index=0
+array.each do |name|
+array[index]=badge_maker(name)
+index +=1
+end
+end
+
+puts assign_rooms(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+
+def printer (array)
+
+assign_rooms(array).each do |rooms|
+puts rooms
+end
+
+batch_badge_creator(array).each do |rooms|
+puts rooms
+end
+
+end
