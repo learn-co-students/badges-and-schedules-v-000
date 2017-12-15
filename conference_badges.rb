cdbcd94f878c0b56648e37d706ae1086ccfa9642
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-12-15 22:02:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,16 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index do
-    |name, index| "Hello, #{name}! You'll be assigned to room #{index}!"
+  attendees.each_with_index.map do |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+    end
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+    end
+
 end
