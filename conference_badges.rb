c68e6e8ce43f3ab3b72ffde47d5decdc94a45226
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-04 14:04:06 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(array)
+  array.each do |element| formatted_batch <<  "Hello, my name is #{element}." end
+    return formatted_batch
+end
+
+def batch_badge_creator(array)
+    batch_array = []
+    array.each do |element| batch_array <<  "Hello, my name is #{element}." end
+    batch_array
+end
+
+array = ["tim", "jim"]
+badge_maker(array)
