7bcc5344a9cfdd96d25d3b0ffff1a9fcb2273e16
jacobsilver2 <jacobsilver2@mac.com>
2018-01-10 10:58:22 -0500

    completed the lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def badge_maker(name)
+   msg = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+   msgList = []
+   names.each do |name|
+      msg = "Hello, my name is #{name}."
+      msgList << msg
+   end
+   msgList
+end
+
+def assign_rooms(names)
+   roomList = []
+   names.each_with_index do |name, i|
+      msg = "Hello, #{name}! You'll be assigned to room #{i+1}!"
+      roomList << msg
+   end
+   roomList
+end
+
+def printer(names)
+   msgList = batch_badge_creator(names)
+   roomList = assign_rooms(names)
+   msgList.each do |msg|
+      puts msg
+   end
+   roomList.each do |msg|
+      puts msg
+   end
+end
+
