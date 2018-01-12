b1758fa96a260afdf73cb967315c892fcbbebff2
Berkeley Fahrenthold <berkeley_fahrenthold@urmc.rochester.edu>
2018-01-12 19:52:13 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(attendees)
+  attendees.map.with_index(1) do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |string|
+    puts string
+  end
+
+  assign_rooms(attendees).each do |string|
+    puts string
+  end
+end
