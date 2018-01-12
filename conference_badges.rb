68a91a0d54bee455e49a925765326d2723911955
jljardon <github email address>
2018-01-11 21:23:49 -0500

    Added methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,17 +4,15 @@ end
 
 def batch_badge_creator(attendees)
    badges = []
-   attendees.each { |name| badges.push "Hello, my name is #{name}." }
+   attendees.each { |name| badges.push(badge_maker(name)) }
    badges
 end
 
 def assign_rooms(attendees)
-   room_counter = 1
    room_assignment = []
-   attendees.each { |name|
-      room_assignment.push"Hello, #{name}! You'll be assigned to room #{room_counter}!"
-      room_counter += 1
-   }
+   attendees.each_with_index do |name, i|
+      room_assignment.push "Hello, #{name}! You'll be assigned to room #{i + 1}!"
+   end
    room_assignment
 end
 
