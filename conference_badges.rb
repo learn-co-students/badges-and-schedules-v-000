c2b70aa528371f82377579c55b07951c0864dc9c
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-01-26 20:35:55 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speaker_list)
+  message_array = speaker_list.collect {
+    |name| "Hello, my name is #{name}."
+  }
+
+  return message_array
+end
+
+def assign_rooms(speaker_list)
+  room_asn = speaker_list.collect {
+    |name| "Hello, #{name}! You'll be assigned to room #{speaker_list.find_index(name) + 1}!"
+  }
+
+  return room_asn
+end
+
+array = ["Joe", "John", "Jane"]
+
+def printer(speaker_list)
+  message_array = batch_badge_creator(speaker_list)
+  room_asn = assign_rooms(speaker_list)
+
+  count = 0
+  while count < (speaker_list.length)
+    puts message_array[count]
+    puts room_asn[count]
+    count += 1
+  end
+end
+
+printer(array)
