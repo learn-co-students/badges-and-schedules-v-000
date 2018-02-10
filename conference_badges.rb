d594d51bd4a95b7c8f90339c8b5c0ee670fd12e0
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-10 00:20:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,44 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  list = []
+  name.each {|n| list << "Hello, my name is #{n}."}
+  list
+end
+
+def assign_rooms(chart)
+  list = []
+  chart.each_with_index do |a, b|
+    list<<"Hello, #{a}! You'll be assigned to room #{b+1}!"
+  end
+  list
+end
+
+def printer(a)
+  batch_badge_creator(a).each {|strings| puts "#{strings}"}
+  assign_rooms(a).each {|strings| puts "#{strings}"}
+end
+
+
+
+
+
+
+
+
+
+
+#
+# def assign_rooms(list)
+#   array = []
+#   list.each_with_index do |name, room|
+#   array.push("Hello, #{name}! You'll be assigned to room #{room + 1}!")
+#   end
+# end
+#
+# def printer(names)
+#   batch_badge_creator(names).each {|badge| puts "#{badge}"}
+#   assign_rooms(names).each {|greeting| puts "#{greeting}"}
+# end
