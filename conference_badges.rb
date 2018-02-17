20520c9a5ad9178e47a1c66b6049219faa06f496
Kayla Cross <kac288@georgetown.edu>
2018-02-17 16:45:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(array)
+  array.collect.with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |element|
+    puts element
+  end
+  assign_rooms(array).each do |element|
+    puts element
+  end
+end
