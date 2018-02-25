f6e0ac89da73ff09972117ed21adbaaf78a374dd
Brett Weisberg <weisberg.brett@gmail.com>
2018-02-25 22:11:36 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(names)
+  return "Hello, my name is #{names}."
+end
+
+def batch_badge_creator(names)
+  names.each do |name|
+    return "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(names)
+  counter = 1
+  names.each do |name|
+  return "Hello, #{name}! You'll be assigned to room #{counter}!"
+  counter += 1
+  end
+end
