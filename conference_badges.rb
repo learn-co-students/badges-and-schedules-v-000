3c951fb05e55d1c8f9c40706ac1646357ea4bae9
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-08 20:02:33 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,14 @@ end
 def assign_rooms(speakers)
   speakers.each_with_index.collect { |name, index_number| "Hello, #{name}! You'll be assigned to room #{index_number + 1}!"}
 end
+
+def printer(names)
+  array = batch_badge_creator(names)
+  array.each do |line|
+  puts line
+end
+  rooms = assign_rooms(names)
+  rooms.each do |place|
+    puts place
+  end
+end
