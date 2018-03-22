37c6886af7b1a1bc0dd44a43969d53749b6b7a27
Vanessa Toro <vanetoro84@gmail.com>
2018-03-22 23:36:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,5 +21,10 @@ return room_number
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
+  batch_badge_creator(attendees).each do |i|
+    puts i 
+  end
+  assign_rooms(attendees).each do |j|
+    puts j
+  end
 end 
