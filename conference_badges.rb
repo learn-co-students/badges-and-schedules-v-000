a671169fea410c22c55b44b2d1ea83b11da97b5d
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-26 22:47:13 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+  messages = []
+  names.each { | name | messages << badge_maker(name)}
+  messages
+end
+
+def assign_rooms ( speakers )
+  assignments = []
+  speakers.each_with_index { |speaker,i| assignments << "Hello, #{speaker}! You'll be assigned to room #{i+1}!"}
+  assignments
+end
+
+def printer ( people )
+  batch_badge_creator(people).each { |badge| puts badge }
+  assign_rooms(people).each { |assignment| puts assignment }
+end
