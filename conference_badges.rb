a5e10b8f6335d24d6c078c476f7b43277cc1bfa5
Nicholas Bharucha <nickbharucha@ou.edu>
2018-04-10 02:29:52 +0000

    Added several methods to the conference_badges.rb file

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  new_array = []
+  speakers.each do |name|
+    phrase = "Hello, my name is #{name}."
+    new_array << phrase
+  end
+  new_array
+end
+
+def assign_rooms(speakers)
+  new_array = []
+  speakers.each_with_index do |speaker, index|
+    phrase = "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    new_array << phrase
+  end
+  new_array
+end
+
+def printer(speakers)
+  badges = batch_badge_creator(speakers)
+  rooms = assign_rooms(speakers)
+  badges.each_with_index do |badge, index|
+    puts badge
+    puts rooms[index]
+  end
+end
