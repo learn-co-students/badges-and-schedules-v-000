da6f96cf4fad12207346539afba683f7440824c7
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-18 16:34:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+
+def conference_badges
+  name = "Arel"
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badges = Array.new
+  room_assignments = Array.new
+  badges_and_room_assignments = Array.new
+end
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do ||
+  end
+end
+
+def assign_rooms(attendees)
+end
+
+def printer
+end
