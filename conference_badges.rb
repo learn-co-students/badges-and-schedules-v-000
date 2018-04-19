40d4ed6a6b1dc0480083198da4d91420b65673ff
Christia T <christiatidwell@bellsouth.net>
2018-04-19 14:16:18 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+def batch_badge_creator(speakers)
+
+end
+
+counter = 0
+def assign_rooms(speakers, counter)
+  speakers.each do |speakers|
+    counter += 1
+    "Hello, #{speakers}! You'll be assigned to room #{counter}!" 
+  end
+end
