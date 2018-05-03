4743781580d970dc53d622b45fe7b574c4c24313
Dale Terrell <tragicid@gmail.com>
2018-05-03 08:11:08 +0000

    Done.

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'conference_badges' do
 
+
   let(:name) {"Arel"}
   let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
   let(:badges) {[
