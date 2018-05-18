89dc26bde3ce92a929f28f146b04ac83017fa82e
Mehwish Aziz <mehwisha@msn.com>
2018-05-18 20:25:26 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+def batch_badge_creator(names)
+  names =[]
+puts "Hello, my name is #{names}"
+end
+def assign_rooms
+
+end
+def printer
+
+end
