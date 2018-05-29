ac5e13cdbcccc678918ffc7c408f377d5ca467ba
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-29 13:14:20 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,12 +16,16 @@ def assign_rooms(attendees)
   room = 0
   attendees.collect do | name |
   room +=1
-  "Hello, #{name}!  You'll be assigned to room #{room}!"
+  "Hello, #{name}! You'll be assigned to room #{room}!"
 end
 end
 
 
-def printer
-  puts batch_badge_creator
-  puts assign_rooms
+def printer(attendees)
+  batch_badge_creator(attendees).each do | printed_tag |
+    puts printed_tag
+  end
+  assign_rooms(attendees).each do | printed_tag|
+    puts printed_tag
+  end
 end
