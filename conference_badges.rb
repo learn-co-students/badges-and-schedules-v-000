562d4e542c9796d7f432bd782ec4b768338ecb91
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-05-31 07:11:53 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+# Make a badge
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_list)
+  name_list.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(speaker_list)
+  assigned = []
+  speaker_list.each_with_index {|speaker, index| assigned.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")}
+  return assigned
+end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def printer(attendees)
+
+  badges = batch_badge_creator(attendees)
+  badges.each {|e| puts e}
+
+  assignments = assign_rooms(attendees)
+  assignments.each {|e| puts e}
+end
