6f011059567b1233a9fe1c13f7405ef7ee281a70
Ana Bebic <anabebic.x@gmail.com>
2018-06-28 12:08:26 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(array)
+  array=[Edsger,Ada, Charles, Alan, Grace, Linus, Matz]
+  
+  array.each do|name|
+  puts "Hello, my name is #{name}."
+end
+
+
+
+end
