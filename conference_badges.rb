e524926ad55b84d92796ab7f729a155266aa97ee
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-10 01:25:54 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,17 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  arr = []
+  attendees = []
   array.each do |greeting|
-    arr << "Hello, my name is #{greeting}."
+    attendees << "Hello, my name is #{greeting}."
+    end
+  return attendees
+end
+
+def assign_rooms(attendees)
+  arr = []
+  attendees.each_with_index do |person, index|
+    arr << "Hello, #{person}! You'll be assigned to room #{index + 1}!"
   end
   return arr
 end
