037a85a6cb137243c95828bda2946ccf31bf4f0d
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-10 01:14:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arr = []
+  array.each do |greeting|
+    arr << "Hello, my name is #{greeting}."
+  end
+  return arr
+end
