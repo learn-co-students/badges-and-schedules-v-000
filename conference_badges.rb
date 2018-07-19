355fae7a87c36987f635a19cca808b2c6ff882db
Rafael Lopez <lpz.raf@gmail.com>
2018-07-19 02:26:18 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+
+def badge_maker(name)
+   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  return badges
+end
+
+
+# brothers = ["Tim", "Tom", "Jim"]
+# brothers.each do |hippo|
+#   puts "Stop hitting yourself #{hippo}!"
+# end
+
+
+# def square_array(array)
+#   num = []
+#   array.each do |number|
+#   num.push(number * number)
+#   end
+#   num
+# end
