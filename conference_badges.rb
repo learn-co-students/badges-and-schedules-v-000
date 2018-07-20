4d50d6b3dd775942efa80b8799170af01e1bd3ce
Rafael Lopez <lpz.raf@gmail.com>
2018-07-20 00:10:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,18 @@ end
 def batch_badge_creator(attendees)
   attend = []
   attendees.each do |name|
-   attend.push(badge_maker(name))
+  attend.push(badge_maker(name))
 end
 attend
+end
+
+def assign_rooms(attendees)
+    x = []
+    attendees.each_with_index do |name, room| 
+    return "Hello, #{name}! You'll be assigned to room #{room.next}!"
+    x.push(attendees.each_with_index)
+  end
+    x
 end
 
 
