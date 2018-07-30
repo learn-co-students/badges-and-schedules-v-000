40ead8101a5384358d101ab1e0e6f85e1715dcf7
Alison Shin <alisonyshin@gmail.com>
2018-07-30 03:02:25 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_list)
+   names_list.collect {|name| badge_maker(name)}
+end
+
+
+
+def assign_rooms(names_list)
+  names_list.each_with_index.collect{|name,index|
+    "Hello, #{name}! You'll be assigned to room #{index +1}!" 
+  }
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |room_assignment|
+    puts room_assignment
+  end
+end
+
+#printer(["Alison","Ali","Alishin"])
