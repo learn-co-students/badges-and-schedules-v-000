704122aae9466bc108d23442c0f4d882d59f0fcd
J Fern <jfern20@gmail.com>
2018-08-01 03:20:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,25 @@
-def badge_maker(name)
-  puts "Hello, my name is #{name}."
  
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+ end 
+
+def batch_badge_creator(names)
+ names.map{ |name| badge_maker(name) }
+ end
+#should return a list of badge messages
+
+def assign_rooms(speakers)
+  new_arr = []
+  speakers.each_with_index do |name, i| 
+    new_arr << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+   end  
+  new_arr
 end 
-badge_maker("Arel")
+#should return a list of welcome messages and room assignments
+
+def printer(attendees)
+  batch_badge_creator(attendees).each{|badge| puts badge}
+  assign_rooms(attendees).each{|rooms| puts rooms}
+  
+end
+#should puts the list of badges and room_assignments
