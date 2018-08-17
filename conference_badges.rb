53ca9918fa1bc75a9eda23a69458b5a06ef54f32
Laura Darlak <laura@polyporedesign.com>
2018-08-17 15:04:13 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,9 @@
 # Write your code here.
 require 'enumerator'
 
+# must define attendees variable!!!
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 # badge maker
 def badge_maker(name)
   "Hello, my name is #{name}."
@@ -8,17 +11,17 @@ end
 
 # batch badge creator
 
-def batch_badge_creator(array)
+def batch_badge_creator(attendees)
   batch = []
-  array.each do |name|
+  attendees.each do |name|
     batch << "Hello, my name is #{name}."
   end
   batch
 end
 
 # assign rooms
-def assign_rooms(speakers)
-  speakers.enum_for(:each_with_index).collect do |attendee, index|
+def assign_rooms(attendees)
+  attendees.enum_for(:each_with_index).collect do |attendee, index|
     room = index + 1
     "Hello, #{attendee}! You'll be assigned to room #{room}!"
   end
@@ -26,17 +29,7 @@ end
 
 # printer
 
-# def printer(attendees)
-#   batch_badge_creator(array).collect {|badge| puts "#{badge}"}
-#   assign_rooms(speakers).collect {|room| puts "#{room}"}
-# end
-
-def printer(array)
-  array.each do |name|
-    puts badge_maker(name)
-  end
-  rooms = assign_rooms(array)
-  rooms.each do |room|
-    puts room
-  end
+def printer(attendees)
+  batch_badge_creator(attendees).each {|badge| puts "#{badge}"}
+  assign_rooms(attendees).each {|room| puts "#{room}"}
 end
