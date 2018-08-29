4e90a680e15c87d8e07c392bcb4cc2757bccb6b5
Ashley Wong <ashwong04@gmail.com>
2018-08-29 14:45:59 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,11 +13,15 @@ end
 
 
 def assign_rooms(names)
+  new_arr = []
   names.each_with_index do |name, index|
-    
+    new_arr << "Hello, #{name}! You'll be assigned to room #{index+=1}!"
+  end
+    new_arr
 end
 
 
-def printer
-  
+def printer(attendees)
+  batch_badge_creator(attendees).each {|attends| puts attends}
+  assign_rooms(attendees).each {|attend| puts attend}
 end
