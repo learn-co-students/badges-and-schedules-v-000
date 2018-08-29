27e140c8d1aeb84d2ae897abdcff1be364ca15c8
Ashley Wong <ashwong04@gmail.com>
2018-08-29 00:13:45 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  new_arr = []
+  names.each {|name| new_arr.push("Hello, my name is #{name}.")}
+  new_arr
+end
