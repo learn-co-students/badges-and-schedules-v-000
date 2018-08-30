20021e1d42f5ad6e4722699e167dee1213ed48ff
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-30 15:43:29 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
+def badge_maker(name)
+  name.each do |badge|
+  puts "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  
+end
+
+def assign_rooms()
+  
+end
+
 # Write your code here.
