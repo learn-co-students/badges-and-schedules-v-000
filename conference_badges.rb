c8194e877a9cec0d3d466b834bb846b1d30952a7
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 00:12:09 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+batch_badge_creator(array)
+  for i in array
+  name = i
+  room = i.each_with_index + 1
+  "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
