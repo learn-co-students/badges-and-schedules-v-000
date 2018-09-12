e22e2fd4cf8c488db2df863a4c35f36a8cad1c62
Stef Claus <stef.claus@gmail.com>
2018-09-12 01:02:59 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  new_array = []
+  attendees.each do |name|
+    badge = "Hello, my name is #{name}."
+  new_array << badge 
+  end 
+  return new_array 
+end 
+
+def assign_rooms(speakers)
+  new_array = []
+  attendees.each do |name|
+    badge = "Hello, my name is #{name}."
+end 
