e584ddde59bd143e8a18da77feb2da843fa75720
Sherry Martin <sm674@cornell.edu>
2018-09-12 23:45:10 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_array)
+   badges_array = []
+   names_array.each do |name|
+   badges_array << badge_maker(name)
+end
+   badges_array
+end
+
+def assign_rooms(speakers, room)
+  rooms = [1-7]
+  puts "Hello, _____! You'll be assigned to room _____!"
