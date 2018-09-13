f1188be397a40a9323fb92d69450731db725b013
Sherry Martin <sm674@cornell.edu>
2018-09-13 19:23:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,25 @@
 # Write your code here.
 def badge_maker(name)
- "Hello, my name is #{name}."
+ "Hello, my name is #{name}."  #do block, if statement, def = ends
 end
 
-def batch_badge_creator(names_array)
-   badges_array = []
+ def batch_badge_creator(names_array) #comment out command forward slash or ctrl
+   badges_array = []  #clear 
    names_array.each do |name|
    badges_array << badge_maker(name)
-end
+ end
    badges_array
-end
+ end
+ 
+ #alternate way to do the above
+ # def batch_badge_creator(speakers)
+ # speakers.map do |name|
+ #"Hello, my name is #{name}."
+ #end
+ #end
 
-def assign_rooms(speakers, room)
-  rooms = [1-7]
-  puts "Hello, _____! You'll be assigned to room _____!"
+# def assign_rooms(speakers, room)
+#   rooms = [1-7]
+#   puts "Hello, _____! You'll be assigned to room _____!"
+  
+  
