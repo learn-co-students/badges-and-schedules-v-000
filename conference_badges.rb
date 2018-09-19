94032082dc2fdb55edca0d6705a65a9b53fcc2f3
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-19 01:54:15 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+    attendees = []
+    attendees.each do |name|
+    "Hello, my name is #{name}."
+  end
+end 
+
+def assign_rooms(name, room)
+  each_with_index enumerator method 
+end 
+
+def print(batch_badge_creator, assign_rooms)
+end 
