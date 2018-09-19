c7780d3c4ba2cec56989cbe902fd99da8460c081
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-19 13:54:20 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,3 +22,25 @@ def printer(attendees)
   batch_badge_creator(attendees).each {|line| puts line}
   assign_rooms(attendees).each {|line2| puts line2}
 end
+
+# def batch_badge_creator(attendees)
+#   attendees.map do |attendee|
+#     "Hello, my name is #{attendee}."
+#   end
+# end
+
+# def assign_rooms(attendees)
+#   attendees.each_with_index.map do |attendee, index|
+#     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+#   end
+# end
+
+# def printer(attendees)
+#   batch_badge_creator(attendees).each do |badge|
+#     puts badge
+#   end
+
+#   assign_rooms(attendees).each do |assignment|
+#     puts assignment
+#   end
+# end
