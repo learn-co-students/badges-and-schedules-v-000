493f6eab262df395edad4738fdbc0f269b9dd0c4
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-01 02:45:35 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+new_array = []
+ attendees.each { |badge_message| new_array << badge_maker(badge_message) }
+ new_array
+end
+
+def assign_rooms(speakers)
+  
+end
