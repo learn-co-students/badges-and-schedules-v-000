22d5e738346ec8d738ae2c77dcac1432cf983727
ktochow1 <ktochow1@binghamton.edu>
2018-10-02 18:17:17 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arr = []
+  array.each { |i| arr << "Hello, my name is #{i}." }
+  arr
+end
+
+ def assign_rooms(list_of_speaker_names)
+      room_assignments_holder = []
+      list_of_speaker_names.each_with_index { |name, room_number| room_assignments_holder << "Hello, #{name}! You'll be assigned to room #{room_number + 1}!"}
+      return room_assignments_holder
+    end
+
+ def printer(list_of_speaker_names)
+      batch_badge_creator(list_of_speaker_names).each { |badge| puts badge}
+      assign_rooms(list_of_speaker_names).each { |room_assignment| puts room_assignment }
+    end
