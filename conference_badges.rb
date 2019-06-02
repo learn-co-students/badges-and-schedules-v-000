504a47893d202aa5d88d11e43bd9e69d3deae41e
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 00:59:28 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+#Edsger, Ada, Charles, Alan, Grace, Linus, and Matz.
+def batch_badge_creator(names)
+    return names.map { |badge| badge_maker(badge) }
+  end
+
+def assign_rooms(attendees)
+attendees.each_with_index { |name, index|
+attendees[name] = index.to_i }
+
+  return "Hello, #{name}! You'll be assigned to #{index}!"
+end
